057f48abfcc24cbb69e97d7ebf3dff446bbb7b11
Nathan Reuter <nathan0reuter@gmail.com>
2018-10-19 09:19:01 -0300

    sdsa

--- main.R
+++ main.R
@@ -14,14 +14,8 @@ processSheet = processSheet[7: length(processSheet)-1];
 # Get all companies name, this will be use later
 allCompanies = gdfpd.get.info.companies(type.data = "companies")[[1]];
 # Test info to get ONE companie info
-#problematicCompanies = c(239, 212, 197, 179, 161, 199, 205);
-problematicCompanies = c("COSAN SA INDUSTRIA E COMERCIO", "COMPANHIA PROVIDENCIA IND E COMERCIO", 
-"COBRASMA SA", "CIA ESTADUAL DE GERACAO E TRANSMISSAO DE ENERGIA ELETRICA", "CENTRO DE IMAGEM DIAGN?STICOS S.A.",
-"COMPANHIA BRASILEIRA DE DISTRIBUI??O", "COMPANHIA DE SANEAMENTO DE MINAS GERAIS", "INDS J B DUARTE SA",
-"CONST SULTEPA SA - EM RECUPERA??O JUDICIAL");
 
-name.companies <- allCompanies[1:400];
-#name.companies <- setdiff(name.companies, problematicCompanies)
+name.companies <- allCompanies[500:600];
 first.date <- '2010-01-01';
 last.date <- '2017-01-01';
 df.statements <- gdfpd.GetDFPData(name.companies = name.companies,first.date = first.date);
