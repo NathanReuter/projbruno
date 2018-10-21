f44acaf2fa6a7bfbc0bf1a57f8d5e4642e78b65a
Nathan Reuter <nathan0reuter@gmail.com>
2018-10-21 17:03:14 -0300

    sdads

--- main.R
+++ main.R
@@ -22,15 +22,6 @@ last.date <- '2017-01-01';
 df.statements <- gdfpd.GetDFPData(name.companies = name.companies,first.date = first.date);
 codeAndName = read.csv("./codeAndName.csv"); 
 
-#planilha1 <- p1(processSheet);
-
-#planilha2 <- p2(df.statements);
-#View(planilha2)
-try({
-  planilha3 <- p3(df.statements)
-  View(planilha3)
-  write.csv(planilha3, file = "./Planilha 3.csv");
-})
 try({
   planilha5 <- p5(df.statements);
   View(planilha5)
@@ -39,27 +30,5 @@ try({
   View(planilha6);
   write.csv(planilha6, file = "./Planilha 6.csv");
 })
-try({
-  planilha7 <- p7(df.statements);
-  View(planilha7);
-  write.csv(planilha7, file = "./Planilha 7.csv");
-  planilha4 <- p4(df.statements, planilha7);
-  View(planilha4);
-  write.csv(planilha4, file = "./Planilha 4.csv");
-})
-try({
-  planilha8 <- p8(df.statements);
-  View(planilha8)
-  write.csv(planilha8, file = "./Planilha 8.csv");
-})
-try({
-  planilha9 <- p9(df.statements);
-  View(planilha9)
-  write.csv(planilha9, file = "./Planilha 9.csv");
-})
-try({
-  planilha11 <- p11(df.statements);
-  View(planilha11);
-  write.csv(planilha11, file = "./Planilha 11.csv");
-})
+
 
